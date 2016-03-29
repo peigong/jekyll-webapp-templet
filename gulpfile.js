88870cc3ac863fb009539fa013e79e9d353e4d49
周培公 <peigong@foxmail.com>
2016-03-29 20:33:58 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,34 @@ var gulp = require('gulp');
 var del = require('del');
 var runSequence = require('run-sequence');
 
+// 清理目标目录
 gulp.task('build:clean', function(cb){
-    del('./dist', cb);
+    del(['./dist'], cb);
 });
+
+// 复制jekyll-blog-app博客应用初始模板
 gulp.task('build:blog', function(){
-    gulp.src('./src/*')
+    gulp.src('./src/**')
     .pipe(gulp.dest('./dist'));
 });
+
+// 复制jekyll-blog-app博客应用配置样例
 gulp.task('build:settings', function(){
     var base = './bower_components/jekyll-settings-sample/settings';
-    gulp.src(base + '/*', { base: base })
+    gulp.src(base + '/**', { base: base })
     .pipe(gulp.dest('./dist'));
 });
-gulp.task('build:settings', function(){
-    var base = './bower_components/jekyll-settings-sample/settings';
-    gulp.src(base + '/*', { base: base })
+
+// 复制Jekyll模板
+gulp.task('build:jekyll', function(){
+    var base = './bower_components/jekyll-blog-templet/jekyll';
+    gulp.src(base + '/**', { base: base })
     .pipe(gulp.dest('./dist'));
 });
+
+// 执行构建任务
 gulp.task('build', function(cb){
-    runSequence('build:clean', ['build:blog', 'build:settings'], cb);
+    runSequence('build:clean', 'build:blog', ['build:settings', 'build:jekyll'], cb);
 });
 
 gulp.task('default', ['build']);

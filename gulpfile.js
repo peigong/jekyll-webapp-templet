707739ce63d59dbe701d25bb51ab492061ac0a0e
周培公 <peigong@foxmail.com>
2016-03-29 10:22:21 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
 var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('clean', function(){
-    del('./dist');
+gulp.task('build:clean', function(cb){
+    del('./dist', cb);
 });
-
-gulp.task('build', ['clean'], function(){
+gulp.task('build:blog', function(){
     gulp.src('./src/*')
     .pipe(gulp.dest('./dist'));
+});
+gulp.task('build:settings', function(){
+    var base = './bower_components/jekyll-settings-sample/settings';
+    gulp.src(base + '/*', { base: base })
+    .pipe(gulp.dest('./dist'));
+});
+gulp.task('build:settings', function(){
+    var base = './bower_components/jekyll-settings-sample/settings';
+    gulp.src(base + '/*', { base: base })
+    .pipe(gulp.dest('./dist'));
+});
+gulp.task('build', function(cb){
+    runSequence('build:clean', ['build:blog', 'build:settings'], cb);
 });
 
 gulp.task('default', ['build']);
